35e2ce01271e9b03d1092f9845f0502ed6fd7f05
ABHISHEK LELLAPALLI <185485285+Abhisheklellapalli@users.noreply.github.com>
2025-08-05 14:28:33 +0530

    Create fork_none.sv

---
+++ fork_join/fork_none.sv
@@ -0,0 +1,26 @@
+module tb;
+  task one();
+    $display("task1 is started at :%0t",$time);
+    #30;
+    $display("task1 is completed at :%0t",$time);
+  endtask
+  task two();
+    $display("task2 is started at :%0t",$time);
+    #50;
+    $display("task2 is completed at :%0t",$time);
+  endtask
+  task three();
+    $display("task3 is started at :%0t",$time);
+    #60;
+    $display("task3 is completed at :%0t",$time);
+  endtask
+  initial 
+    begin
+      fork
+        one();
+        two();        
+      join_none;
+      three();
+    end
+endmodule
+    
