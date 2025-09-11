5cc05eb2f9bc0779a2ac711af94f92521baebb81
ABHISHEK LELLAPALLI <185485285+Abhisheklellapalli@users.noreply.github.com>
2025-09-11 15:59:45 +0530

    Update Driver.sv

--- on Transactions/half adder/Driver.sv
+++ on Transactions/half adder/Driver.sv
@@ -1,13 +1,16 @@
 // Driver
 class driver;
-  trans tx;
-  mailbox mbx;
-  virtual fa intrf;
-  task go;
-    mbx.get(tx);
-    intrf.a=tx.a;
-    intrf.b=tx.b;
-    #1;
-    $display(" driver values a=%b b=%b | sum:%b cout:%b ",intrf.a,intrf.b,intrf.sum,intrf.cout);
-  endtask
+trans tx;
+mailbox mbx;
+virtual fa intrf;
+task go;
+  forever 
+    begin
+  mbx.get(tx);
+  intrf.a=tx.a;
+  intrf.b=tx.b;
+  #1;
+  $display(" driver values a=%b b=%b | sum:%b cout:%b ",intrf.a,intrf.b,intrf.sum,intrf.cout);
+    end
+endtask
 endclass
