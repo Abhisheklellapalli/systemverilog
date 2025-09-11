3d3a09a00f1f6ddb03800fd5b1fc813890a742ec
ABHISHEK LELLAPALLI <185485285+Abhisheklellapalli@users.noreply.github.com>
2025-09-11 15:59:02 +0530

    Update Generator.sv

--- on Transactions/half adder/Generator.sv
+++ on Transactions/half adder/Generator.sv
@@ -1,11 +1,13 @@
 // Generator
 class generator;
-  trans tx;
-  mailbox mbx;
-  task go;
-    tx=new();
-      assert(tx.randomize());
-      mbx.put(tx);
-      tx.print();
-  endtask
+trans tx;
+mailbox mbx;
+task go;
+  repeat(4) begin
+  tx=new();
+    assert(tx.randomize());
+    mbx.put(tx);
+    tx.print();
+  end
+endtask
 endclass
